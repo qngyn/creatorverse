fb97ac383bec407ce456568958166f9f9712dbc8
Quynh Nguyen <qtn2@case.edu>
2023-06-11 14:03:12 -0400

    Initialize project and connect to supabase db

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const URL = 'https://arictkdglhjspdlushvy.supabase.co';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyaWN0a2RnbGhqc3BkbHVzaHZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYyODI1NzksImV4cCI6MjAwMTg1ODU3OX0.ZAyB9lH96nndkkRsgIIwxW-QI8TLy_os1M20KLa_ODQ';
+const supabase = createClient(URL, API_KEY);
+
+export const supabase = createClient(URL, API_KEY);
