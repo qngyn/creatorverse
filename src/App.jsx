8305248a6acfc216f899f61c0e73d4ad83238ec5
Quynh Nguyen <qtn2@case.edu>
2023-06-20 01:06:16 -0400

    Add youtube url icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@picocss/pico'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/creator-card.jsx
+++ src/components/creator-card.jsx
@@ -1,11 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faYoutube, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, faYoutube, faFontAwesome);
+
 const CreatorCard = ({ creator }) => {
     const { name, description, url, imageURL } = creator;
 
     return (<>
-        <p>{name}</p>
+        <article style={{ width: '300px', height: '200px', backgroundImage: `url(${imageURL})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: '#333333', backgroundBlendMode: 'multiply' }} >
+            <h5 style={{ textAlign: 'left', marginBottom: '0' }}>{name}</h5>
+
+            <a href={url}>
+                <FontAwesomeIcon icon={faYoutube} />
+            </a>
+
+            <hr />
+
+            <p style={{ textAlign: 'left', fontSize: '12px' }}>{description}</p>
+        </article >
+
+        {/* <p>{name}</p>
         <p>{description}</p>
         <p>{url}</p>
-        <image src={imageURL} />
+        <image src={imageURL} /> */}
     </>)
 }
 
