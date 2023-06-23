feda939c40d7622f158baaa6009defa7a4f0a44a
Quynh Nguyen <qtn2@case.edu>
2023-06-23 00:03:42 -0400

    Adjust card styling

--- src/components/creator-card.jsx
+++ src/components/creator-card.jsx
@@ -4,6 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { faYoutube, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 import { Link } from "react-router-dom";
+import '../App.css';
 
 library.add(fas, faArrowRight, faYoutube, faFontAwesome);
 
@@ -11,20 +12,23 @@ const CreatorCard = ({ creator }) => {
     const { id, name, description, url, imageURL } = creator;
 
     return (<>
-        <article style={{ width: '300px', height: '200px', backgroundImage: `url(${imageURL})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: '#333333', backgroundBlendMode: 'multiply' }} >
-            <h5 style={{ textAlign: 'left', marginBottom: '0' }}>{name}</h5>
+        <article style={{ width: '300px', maxHeight: '225px', backgroundImage: `url(${imageURL})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: '#333333', backgroundBlendMode: 'multiply' }} >
+            <div>
+                <h4 style={{ textAlign: 'left', marginBottom: '0' }}>{name}</h4>
 
-            <a href={url}>
-                <FontAwesomeIcon icon={faYoutube} />
-            </a>
+                <a href={url}>
+                    <FontAwesomeIcon icon={faYoutube} />
+                </a>
 
-            <hr />
-            <p className='ellipsis-two-lines' style={{ textAlign: 'left' }}>{description}</p>
-            <Link to={`/view/${id}`}>
-                <p> View more 
-                    <span style={{marginLeft: '5px'}}><FontAwesomeIcon icon={faArrowRight} /></span>
-                </p>
-            </Link>
+                <hr />
+                <p className='ellipsis-two-lines' style={{ textAlign: 'left' }}>{description}</p>
+                <Link to={`/view/${id}`}>
+                    <p> View more
+                        <span style={{ marginLeft: '5px' }}><FontAwesomeIcon icon={faArrowRight} /></span>
+                    </p>
+                </Link>
+            </div>
+
 
         </article >
 
