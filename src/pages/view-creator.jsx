55b7cd92536eba5e47f3b429eb59da23b746f036
Quynh Nguyen <qtn2@case.edu>
2023-06-27 00:07:28 -0400

    Add delete btn in creator card

--- src/pages/view-creator.jsx
+++ src/pages/view-creator.jsx
@@ -3,11 +3,12 @@ import { Link, useParams } from "react-router-dom";
 import { supabase } from "../client";
 import './view-creator.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const ViewCreator = () => {
     const { id } = useParams()
     const [creator, setCreator] = useState();
+    const [deleted, setDeleted] = useState(false);
 
     useEffect(() => {
         console.log('id: ', id);
@@ -28,16 +29,35 @@ const ViewCreator = () => {
         }
     }, [id]);
 
-    useEffect(() => {
-        console.log('check current creator: ', creator);
-    }, [creator]);
-
     if (!creator) {
         return (<h3> Failed to fetch creator or invalid creator id </h3>)
     }
 
+    const onDelete = async () => {
+
+        const { error } = await supabase
+            .from('creators')
+            .delete()
+            .eq('id', id);
+        
+        if (error) {
+            console.error("Failed to delete user: ", error);
+        } else {
+            setDeleted(true);
+        }
+
+    }
+
     const { name, description, url, imageURL } = creator;
 
+    if (deleted) {
+        return (
+            <article>
+                Creator {name} deleted!
+            </article>
+        )
+    }
+    
     return (
         <>
             <div className='container'>
@@ -49,10 +69,18 @@ const ViewCreator = () => {
                         <p>{description}</p>
                         <p>{url}</p>
 
-                        <Link to={`/edit/${id}`} role="button" style={{ height: '20px', display: 'flex', width: 'fit-content', alignItems: 'center' }}>
-                            <p style={{ margin: 0 }}> Edit <span style={{ marginLeft: '5px' }}><FontAwesomeIcon icon={faPenToSquare} /></span>
-                            </p>
-                        </Link>
+                        <div style={{ display: 'flex' }}>
+                            <Link to={`/edit/${id}`} role="button" style={{ height: '40px', display: 'flex', width: 'fit-content', alignItems: 'center', marginRight: '5px' }}>
+                                <p style={{ margin: 0 }}> Edit <span style={{ marginLeft: '5px' }}><FontAwesomeIcon icon={faPenToSquare} /></span>
+                                </p>
+                            </Link>
+
+                            <button className='secondary delete-btn' onClick={onDelete}>
+                                <p style={{ margin: 0 }}> Remove <span style={{ marginLeft: '5px' }}><FontAwesomeIcon icon={faTrash} /></span>
+                                </p>
+                            </button>
+                        </div>
+
                     </div>
                 </div>
             </div>
